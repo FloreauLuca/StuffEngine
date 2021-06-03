620110798ac76b94f5d6803ed437b4b0de596008
FloreauLuca <lucafloreau@akeonet.com>
2021-06-03 16:05:03 +0200

    Add google benchmark and google tests and bubble sort benchmark

--- common/algo_lib/src/bubble_sort.cpp
+++ common/algo_lib/src/bubble_sort.cpp
@@ -39,5 +39,23 @@ std::pair<size_t, size_t> BubbleSort::Update()
 
 void BubbleSort::AutoSort()
 {
+	int i, j;
+	bool swapped;
+	for (i = 0; i < listSize_ - 1; i++)
+	{
+		swapped = false;
+		for (j = 0; j < listSize_ - i - 1; j++)
+		{
+			if (list_[j] > list_[j + 1])
+			{
+				std::swap(list_[j], list_[j + 1]);
+				swapped = true;
+			}
+		}
+
+		// IF no two elements were swapped by inner loop, then break
+		if (swapped == false)
+			break;
+	}
 }
 }
---
+++ main/gtests/test_bubble_sort.cpp
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+
+TEST(Algo, BubbleSort)
+{
+	EXPECT_TRUE(true);
+}
